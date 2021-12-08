430df0342798cb0fcc4f5f9b106be10a0cdd270d
Vladimir-Khlghatyan <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2021-12-09 01:20:47 +0400

    C00
    Adding solutions of Piscine C00.

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkhlghat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 00:20:40 by vkhlghat          #+#    #+#             */
+/*   Updated: 2021/10/17 21:19:40 by vkhlghat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_num2(int a, int b, int c, int d)
+{
+	if ((a * 10 + b) < (c * 10 + d))
+	{
+		ft_putchar(a);
+		ft_putchar(b);
+		ft_putchar(' ');
+		ft_putchar(c);
+		ft_putchar(d);
+		if (a < '9' || b < '8' || c < '9' || d < '9')
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+	}
+}
+
+void	ft_for_while(int a, int b, int c, int d)
+{
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			while (c <= '9')
+			{
+				while (d <= '9')
+				{
+					ft_print_num2(a, b, c, d);
+					d++;
+				}
+				d = '0';
+				c++;
+			}
+			d = '0';
+			c = '0';
+			b++;
+		}
+		d = '0';
+		c = '0';
+		b = '0';
+		a++;
+	}	
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+	int	c;
+	int	d;
+
+	a = '0';
+	b = '0';
+	c = '0';
+	d = '1';
+	ft_for_while(a, b, c, d);
+}
---
+++ C00/ex08/ft_print_combn.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkhlghat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 16:20:51 by vkhlghat          #+#    #+#             */
+/*   Updated: 2021/10/17 21:22:28 by vkhlghat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print(int *t, int qanak)
+{
+	int	i;
+	int	is_valid;
+
+	i = 1;
+	is_valid = 1;
+	while (i < qanak)
+	{
+		if (t[i - 1] >= t[i])
+			is_valid = 0;
+		i++;
+	}
+	if (is_valid)
+	{
+		i = 0;
+		while (i < qanak)
+			ft_putchar(t[i++] + '0');
+		if (t[0] < (10 - qanak))
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+	}
+}
+
+int	ft_initial(int i)
+{
+	while (i < 10)
+	{
+		ft_putchar(i + '0');
+		if (i != 9)
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+		i++;
+	}
+	return (i);
+}
+
+void	ft_print_combn(int n)
+{
+	int	i;
+	int	tab[9];
+
+	i = 0;
+	if (n == 1)
+		i = ft_initial(i);
+	while (i < n)
+		tab[i++] = 0;
+	while (tab[0] <= (10 - n) && n > 1)
+	{
+		ft_print(tab, n);
+		tab[n - 1]++;
+		i = n;
+		while (i && n > 1)
+		{
+			i--;
+			if (tab[i] > 9)
+			{
+				tab[i - 1]++;
+				tab[i] = 0;
+			}
+		}
+	}
+}
