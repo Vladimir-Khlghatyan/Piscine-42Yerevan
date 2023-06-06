88d2f630054f6f6339464da3d5fb1e6b562410d6
Vladimir <vladimirkhlghatyan.m@gmail.com>
2023-06-06 14:59:43 +0400

    binary search

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -6,18 +6,29 @@
 /*   By: vkhlghat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 22:10:01 by vkhlghat          #+#    #+#             */
-/*   Updated: 2022/11/07 02:15:01 by vkhlghat         ###   ########.fr       */
+/*   Updated: 2023/06/06 14:58:30 by vkhlghat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb)
 {
-	int	x;
+	int	start;
+	int	end;
+	int	mid;
 
-	x = 1;
-	while (nb / x > x)
-		x++;
-	if (x * x == nb)
-		return (x);
+	if (nb < 1 || nb > 2147395600)
+		return (0);
+	start = 0;
+	end = 46341;
+	while (end - start > 1)
+	{
+		mid = (start + end) / 2;
+		if (mid * mid == nb)
+			return (mid);
+		if (mid * mid > nb)
+			end = mid;
+		else
+			start = mid;
+	}
 	return (0);
 }
